258f9eadef02ceb71094f16322dd8f118052441a
tomastomvi <vitomas2007@gmail.com>
2025-09-02 22:10:48 -0300

    fist

---
+++ personagem/main.java
@@ -0,0 +1,9 @@
+public class main{
+    public static void main(String[] args) {
+        personagem p1 = new personagem("Aragorn", 100, 50);
+        guerreiro g1 = new guerreiro("Legolas", 120, 40, "Arco e Flecha");
+        mago m1 = new mago("Gandalf", 80, 100, 80);
+        
+
+    }
+}
